002eafb58bea7534c1d03c84631c989130b5fe50
chriskayomb <ibond.chris@gmail.com>
2024-08-26 10:59:28 +0200

    initial commit

---
+++ src/test/java/com/example/springbootjenkins/SpringbootjenkinsApplicationTests.java
@@ -0,0 +1,21 @@
+package com.example.springbootjenkins;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class SpringbootjenkinsApplicationTests {
+
+	public static Logger logger = LoggerFactory.getLogger(SpringbootjenkinsApplicationTests.class);
+
+	@Test
+	void contextLoads() {
+		logger.info("Test case executing ...");
+		assertEquals(true, true);
+	}
+
+}
