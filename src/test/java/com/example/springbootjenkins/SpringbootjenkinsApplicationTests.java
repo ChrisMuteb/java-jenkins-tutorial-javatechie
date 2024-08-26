817f15e17f88a2696384874d8f207cbee53b38d8
chriskayomb <ibond.chris@gmail.com>
2024-08-26 16:01:54 +0200

    fixed issues

--- src/test/java/com/example/springbootjenkins/SpringbootjenkinsApplicationTests.java
+++ src/test/java/com/example/springbootjenkins/SpringbootjenkinsApplicationTests.java
@@ -13,7 +13,7 @@ class SpringbootjenkinsApplicationTests {
 	public static Logger logger = LoggerFactory.getLogger(SpringbootjenkinsApplicationTests.class);
 
 	@Test
-	 contextLoads() {
+	void contextLoads() {
 		logger.info("Test case executing ...");
 		logger.info("Test case executing second log statement...");
 		assertEquals(true, true);
